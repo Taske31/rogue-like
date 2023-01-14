e5b1345fbcd122868b82daf6ec73bfac97e81c97
Stuvar1 <salahovmaksum8@gmail.com>
2023-01-14 23:10:20 +0300

    intro working

--- main.py
+++ main.py
@@ -6,41 +6,73 @@ import hero
 import map
 import mobs
 
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
 if __name__ == '__main__':
     pygame.init()
     window = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
     size = width, height = window.get_size()
     window.fill((255, 255, 255))
     font = pygame.font.Font(None, 40)
+    fon = pygame.transform.scale(load_image('floor.png'), (width, height))
     hero_group = hero.hero_group
     mobs_group = mobs.mobs_group
     clock = pygame.time.Clock()
     FPS = 24
 
+
+    def intro():
+        window.blit(fon, (0, 0))
+        text_coord = 50
+        intro_text = ["ЗАСТАВКА", "",
+                      "Правила игры",
+                      "Если в правилах несколько строк,",
+                      "приходится выводить их построчно"]
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('white'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            window.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            clock.tick(FPS)
+
+
     def terminate():
         pygame.quit()
         sys.exit()
-
-    def load_image(name, colorkey=None):
-        fullname = os.path.join('images', name)
-        # если файл не существует, то выходим
-        if not os.path.isfile(fullname):
-            print(f"Файл с изображением '{fullname}' не найден")
-            sys.exit()
-        image = pygame.image.load(fullname)
-        if colorkey is not None:
-            image = image.convert()
-            if colorkey == -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey(colorkey)
-        else:
-            image = image.convert_alpha()
-        return image
 
     def health_bar(health):
         text = font.render("HP", True, (255, 255, 255))
         window.blit(text, (width - 400, 50))
         pygame.draw.rect(window, (255, 0, 0), (width - 340, 50, health * 2, 30))
+
 
     def attack(receiver, attacker):
         receiver.get_damage(attacker.attack())
