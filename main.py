9b7deb3633043c9010ab34381f02960e8fa188f9
Stuvar1 <118453402+Stuvar1@users.noreply.github.com>
2023-01-14 21:16:01 +0300

    patch leha lox

--- main.py
+++ main.py
@@ -5,37 +5,67 @@ import settings
 import hero
 import map
 import mobs
+import chest
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 if __name__ == '__main__':
     pygame.init()
     window = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
     size = width, height = window.get_size()
     window.fill((255, 255, 255))
+    fon = pygame.transform.scale(load_image('floor.png'), (width, height))
     font = pygame.font.Font(None, 40)
     hero_group = hero.hero_group
     mobs_group = mobs.mobs_group
+    chest_group = chest.chest_group
     clock = pygame.time.Clock()
     FPS = 24
+
+    def intro():
+        window.blit(fon, (0, 0))
+        text_coord = 50
+        intro_text = ["ЗАСТАВКА", "",
+                      "Правила игры",
+                      "Если в правилах несколько строк,",
+                      "приходится выводить их построчно"]
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('white'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            window.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            clock.tick(FPS)
 
     def terminate():
         pygame.quit()
         sys.exit()
 
-    def load_image(name, colorkey=None):
-        fullname = os.path.join('images', name)
-        # если файл не существует, то выходим
-        if not os.path.isfile(fullname):
-            print(f"Файл с изображением '{fullname}' не найден")
-            sys.exit()
-        image = pygame.image.load(fullname)
-        if colorkey is not None:
-            image = image.convert()
-            if colorkey == -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey(colorkey)
-        else:
-            image = image.convert_alpha()
-        return image
 
     def health_bar(health):
         text = font.render("HP", True, (255, 255, 255))
@@ -50,8 +80,10 @@ if __name__ == '__main__':
     hero_health = 100
     boar = mobs.Mob('boar', 20, 30, (500, 200))
     boar2 = mobs.Mob('boar', 20, 30, (700, 800))
+    chest = chest.Chest((500, 500))
     run = True
     BOARTIMER = pygame.USEREVENT + 1
+    intro()
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -67,7 +99,7 @@ if __name__ == '__main__':
                     pygame.time.set_timer(BOARTIMER, 2000)
             if event.type == BOARTIMER:
                 attack(hero_object, boar)
-                # attack(hero_object, boar2)
+                attack(hero_object, boar2)
 
         game_map = map.game_map
 
@@ -83,6 +115,8 @@ if __name__ == '__main__':
 
         hero_group.draw(window)
         mobs_group.draw(window)
+        chest_group.draw(window)
+        chest.open()
 
         health_bar(hero_health)
         pygame.display.flip()
