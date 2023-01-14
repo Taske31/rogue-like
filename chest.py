e12dc3609c78805d897c95d1612bec7e56509d95
Stuvar1 <118453402+Stuvar1@users.noreply.github.com>
2023-01-14 19:54:59 +0300

    chest 8

---
+++ chest.py
@@ -0,0 +1,50 @@
+import pygame
+import sys
+import os
+
+chest_group = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Chest(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(chest_group)
+        self.cur_frame = 0
+        name = 'chest'
+        self.frames_stay = []
+        self.sheet = load_image(f'{name}.png')
+        self.cut_sheet(self.sheet, 2, 1, self.frames_stay)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.image = self.frames_stay[self.cur_frame]
+        self.rect = self.image.get_rect()
+
+    def spawn(self):
+        self.cut_sheet(self.sheet, 2, 1, self.frames_stay)
+
+    def cut_sheet(self, sheet, columns, rows, frame):
+        self.rect = pygame.Rect(500, 500, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                frame.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+print(2)
