8479455e180bbeee6e68e6f0c4142eeea7ed7252
Aleksey <alekseik2017@mail.ru>
2023-01-15 17:35:54 +0300

    norm death

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import map
 import mobs
 import chest
 
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('images', name)
     # если файл не существует, то выходим
@@ -41,18 +42,18 @@ if __name__ == '__main__':
     def death_screen():
         window.blit(fon, (0, 0))
         text_coord = 300
-        intro_text = ["Экран смерти.", "",
+        death_screen_text = ["Экран смерти.", "",
                       "НР вашего персонажа опустилось до 0.",
                       "Старайтесь так не делать.",
                       "*Нажмите на любую клавишу что бы начать новый забег*"]
-        for line in intro_text:
+        for line in death_screen_text:
             string_rendered = font.render(line, True, pygame.Color('white'))
-            intro_rect = string_rendered.get_rect()
+            death_screen_rect = string_rendered.get_rect()
             text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 10
-            text_coord += intro_rect.height
-            window.blit(string_rendered, intro_rect)
+            death_screen_rect.top = text_coord
+            death_screen_rect.x = 500
+            text_coord += death_screen_rect.height
+            window.blit(string_rendered, death_screen_rect)
 
         while True:
             for event in pygame.event.get():
@@ -60,7 +61,7 @@ if __name__ == '__main__':
                     terminate()
                 elif event.type == pygame.KEYDOWN or \
                         event.type == pygame.MOUSEBUTTONDOWN:
-                    return  # начинаем игру
+                    return  terminate()
             pygame.display.flip()
             clock.tick(FPS)
 
@@ -70,13 +71,13 @@ if __name__ == '__main__':
         intro_text = ["Начальный экран.", "",
                       "Убивайте противников, проходите в следующие комнаты.",
                       "Получайте сундуки с новым оружием, веселитесь.",
-                      "*Нажмите на любую клавишу что бы продолжить*"]
+                      "*Нажмите на любую клавишу что бы выйти*"]
         for line in intro_text:
             string_rendered = font.render(line, True, pygame.Color('white'))
             intro_rect = string_rendered.get_rect()
             text_coord += 10
             intro_rect.top = text_coord
-            intro_rect.x = 10
+            intro_rect.x = 500
             text_coord += intro_rect.height
             window.blit(string_rendered, intro_rect)
 
@@ -111,7 +112,8 @@ if __name__ == '__main__':
     boar2 = mobs.Mob('boar', 20, 30, (700, 800))
     chest = chest.Chest((500, 500))
     run = True
-    BOARTIMER = pygame.USEREVENT + 1
+    DEATHTIMER = pygame.USEREVENT + 1
+    BOARTIMER = pygame.USEREVENT
     intro()
     while run:
         for event in pygame.event.get():
@@ -127,6 +129,10 @@ if __name__ == '__main__':
             if event.type == BOARTIMER:
                 attack(hero_object, boar)
                 attack(hero_object, boar2)
+            if hero_health <= 0:
+                pygame.time.set_timer(DEATHTIMER, 500)
+            if event.type == DEATHTIMER:
+                death_screen()
 
         game_map = map.game_map
 
@@ -151,4 +157,3 @@ if __name__ == '__main__':
         window.fill((255, 255, 255))
 
     pygame.quit()
-print()
